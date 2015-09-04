348768a3b6cb16af70f1c6f4767509507ae45538
mcyang <woohoo499@gmail.com>
2015-09-04 17:35:08 +0800

    Add PlayersController and Join 2 tables in the same View.

---
+++ HelloMVC/Controllers/PlayersController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private JohnTestEntities db = new JohnTestEntities();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            
+            var query = from p in db.Players.Where(m => m.IsDelete == false)
+                        join t in db.Teams on p.TeamID equals t.ID
+                        select new {p, t.Name };
+
+            List<PlayersViewModel> list = new List<PlayersViewModel>();
+            foreach (var q in query)
+            {
+                PlayersViewModel playersVM = new PlayersViewModel();
+                playersVM.PlayerID = q.p.ID;
+                playersVM.PlayerName = q.p.Name;
+                playersVM.Height = q.p.Height ?? 0;
+                playersVM.Weight = q.p.Weight ?? 0;
+                playersVM.Age = q.p.Age ?? 0;
+                playersVM.TeamName = q.Name;
+
+                list.Add(playersVM);
+            }
+
+            return View(list);
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Height,Weight,Age,TeamID,IsDelete")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Height,Weight,Age,TeamID,IsDelete")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Players.Find(id);
+            db.Players.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HelloMVC/Models/PlayersViewModels.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloMVC.Models
+{
+    public class PlayersViewModel
+    {
+        public int PlayerID { get; set; }
+        public string PlayerName { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string TeamName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
